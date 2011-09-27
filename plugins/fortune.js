a1e812593a42d217354c942f9d6f65bb585ec508
justin caratzas <justin.caratzas@gmail.com>
2011-09-27 00:20:07 -0400

    fixing whitespace

--- plugins/fortune.js
+++ plugins/fortune.js
@@ -8,26 +8,26 @@ plugin.author = 'justin';
 
 plugin.on('chat', function(bot, msg) {
   var match = msg.text.match(/^\.(fortune-help|fortune)\s?([a-z|0-9|-]+)?/);
-  if(match) {  
+  if(match) {
     var fortune_type = '';
     if (match[2]) {
-      fortune_type = match[2];      
-    } 
-    
+      fortune_type = match[2];
+    }
+
     if (match[1] == 'fortune-help') {
       exec("fortune -f", function (err, stdout, stderr) {
-	if (err) return;
-	var oneline = stderr.replace(/(\r\n|\n|\r)/gm,"").split(' ');
-	var results = [];
-	for ( i = 3; i < oneline.length; i=i+2) {
-	  if(oneline[i].length > 0) { results.push(' '+ oneline[i]); }
+        if (err) return;
+        var oneline = stderr.replace(/(\r\n|\n|\r)/gm,"").split(' ');
+        var results = [];
+        for ( i = 3; i < oneline.length; i=i+2) {
+          if(oneline[i].length > 0) { results.push(' '+ oneline[i]); }
 	}
-	bot.say(msg.channel, results);
+        bot.say(msg.channel, results);
       });
     } else {
-	exec("fortune -s " + fortune_type, function (err, stdout, stderr) {
-	  if (err) return;
-	  bot.say(msg.channel, stdout.replace(/(\r\n|\n|\r)/gm," "));
+        exec("fortune -s " + fortune_type, function (err, stdout, stderr) {
+          if (err) return;
+          bot.say(msg.channel, stdout.replace(/(\r\n|\n|\r)/gm," "));
 	});
     }
   }
